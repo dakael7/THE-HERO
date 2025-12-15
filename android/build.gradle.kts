21c6b96ef8e51012fba265dc0256612a8c55d8ee
Zigg04 <Luisgpetit11@gmail.com>
2025-12-15 13:39:33 -0400

    Icono

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,6 +9,12 @@ allprojects {
     }
 }
 
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
