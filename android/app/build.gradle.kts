990e6577bd513afae6e38869c41119c7304df607
Zigg04 <Luisgpetit11@gmail.com>
2026-03-15 20:50:16 -0400

    keys

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,13 +9,22 @@ plugins {
 }
 
 import java.util.Base64
+import java.util.Properties
+import java.io.FileInputStream
 
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.add("-Xlint:-options")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+val hasReleaseKeystore = keystorePropertiesFile.exists()
+if (hasReleaseKeystore) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.the_hero"
+    namespace = "com.theheroprojects.thehero"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -41,7 +50,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.the_hero"
+        applicationId = "com.theheroprojects.thehero"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -55,11 +64,22 @@ android {
                 ?: ""
     }
 
+    signingConfigs {
+        if (hasReleaseKeystore) {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig =
+                if (hasReleaseKeystore) signingConfigs.getByName("release")
+                else signingConfigs.getByName("debug")
 
             isMinifyEnabled = false
             isShrinkResources = false
--- android/app/src/main/kotlin/com/example/the_hero/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/the_hero/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.the_hero
+package com.theheroprojects.thehero
 
 import android.view.LayoutInflater
 import android.view.View
